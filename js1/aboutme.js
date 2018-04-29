c94ff3f7e787215d44ff4997bfd163b700ade652
Weidi Xie <weidi.xie@eng.ox.ac.uk>
2018-04-29 20:33:57 +0100

    initial commit

--- js1/aboutme.js
+++
@@ -1,5 +0,0 @@
-function init() {
-	$('#aboutme').append(Handlebars.compile( $('#template_aboutme').html() )( aboutME));
-	$('#aboutAwards').append(Handlebars.compile( $('#template_aboutAwards').html() )( aboutAwards));
-}
-init();
--- js1/header.js
+++
@@ -1,7 +0,0 @@
-function init() {
-	document.getElementById("init").innerHTML = starter.init;
-	document.getElementById("intro").innerHTML = starter.intro;
-	document.getElementById("status").innerHTML = starter.status;
-	$('#headeraddress').append(Handlebars.compile( $('#template_header').html() )( header));
-}
-init();
