379c5730ea109305b0afc767003e2b0e82599d5c
Weidi Xie <weidi.xie@eng.ox.ac.uk>
2018-07-03 15:42:33 +0100

    paper update

--- js1/staticdata.js
+++ js1/staticdata.js
@@ -103,7 +103,7 @@ var publication = [
     {
         author: "Davis M. Vigneault*, Weidi Xie*, Carolyn Y. Ho, David A. Bluemke and J. Alison Noble (* indicates equal contribution)",
         title: "Omega-Net: Fully Automatic, Multi-View Cardiac MR Detection, Orientation, and Segmentation with Deep Neural Networks",
-        conf: "In: Medical Image Analysis, Volume 48, August 2018, Pages 95–106. (Impact Factor: 5.012)",
+        conf: "In: Medical Image Analysis, Volume 48, August 2018, Pages 95–106. (* indicates equal contribution, 5-Year Impact Factor: 5.417)",
         pdf: "pdf/omega_nets.pdf",
         website: "https://www.sciencedirect.com/science/article/pii/S1361841518302998",
         award:""
@@ -111,7 +111,7 @@ var publication = [
     {
         author: "Ruobing Huang, Weidi Xie and J. Alison Noble",
         title: "VP-Nets : Efficient Automatic Localization of Key Brain Structures in 3D Fetal Neurosonography",
-        conf: "In: Medical Image Analysis, Volume 47, July 2018, Pages 127–139. (Impact Factor: 5.012)",
+        conf: "In: Medical Image Analysis, Volume 47, July 2018, Pages 127–139. (5-Year Impact Factor: 5.417)",
         pdf: "pdf/vp_nets.pdf",
         website: "https://www.sciencedirect.com/science/article/pii/S1361841518301920",
         award:""
@@ -119,7 +119,7 @@ var publication = [
     {
         author: "Ana I.L. Namburete*, Weidi Xie*, Mohammad Yaqub, Andrew Zisserman and J. Alison Noble (* indicates equal contribution)",
         title: "Fully-Automated Alignment of 3D Fetal Brain Ultrasound to A Canonical Reference Space Using Multi-task Learning",
-        conf: "In: Medical Image Analysis, Volume 46, May 2018, Pages 1-14. (Impact Factor: 5.012)",
+        conf: "In: Medical Image Analysis, Volume 46, May 2018, Pages 1-14. (* indicates equal contribution, 5-Year Impact Factor: 5.417)",
         pdf: "pdf/3d_brain_alignment.pdf",
         website: "https://www.sciencedirect.com/science/article/pii/S1361841518300306",
         award:""
